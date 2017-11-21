3bf7c51901bc7cdd9b64ef52e872a28d5603313c
Ricky Peters <ericrickyallenpeters@gmail.com>
2017-11-21 10:58:20 -1000

    Login navigation works, no backend talk

--- app/views/SecondScreen.js
+++ app/views/SecondScreen.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text, Button, Image } from 'react-native';
+import { ScrollView, Text, Button, Image } from 'react-native';
+import { onSignOut } from '../auth';
 
-export default class SecondScreen extends Component {
-  static navigationOptions = {
-    tabBarIcon: ({tintColor}) => (
-        <Image
-          source={require('../assets/chats-icon.png')}
-          style={{width: 25, height: 25, tintColor: 'black'}}>
-        </Image>
-      )
-  }
-  render () {
-    return <View style={
-      {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-      }
-    }>
-        <Text style={{fontSize: 30}}>
+export default ({ navigation }) => (
+      <ScrollView
+      style={{
+          backgroundColor: "#bcbec1",
+          width: 80,
+          height: 80,
+          borderRadius: 40,
+          alignSelf: "center",
+          marginBottom: 20
+        }}>
+        <Text style={{
+          fontSize: 30,
+          alignItems: "center",
+          justifyContent: "center",}}>
           This is the SECOND  SHADE TAB YO DADDIO
         </Text>
-      </View>
-  }
-}
+        <Button
+        backgroundColor="#03A9F4"
+        title="SIGN OUT"
+        onPress={() => onSignOut().then(() => navigation.navigate("SignedOut"))}
+      />
+      </ScrollView>
+);
