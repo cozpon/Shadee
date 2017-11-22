09c839d1f7a5bf7ea8877a571cf7354a023f83e1
Kevin Yogi <kevinyogi@Kevins-MacBook-Pro.local>
2017-11-21 16:24:07 -1000

    very very basic basic rendering of messages

--- app/views/Feed.js
+++ app/views/Feed.js
@@ -10,6 +10,7 @@ import {
 } from 'react-native';
 import { connect } from 'react-redux';
 import { loadMessages } from '../actions/messages';
+import Message from '../components/Message';
 
 class Feed extends Component {
 
@@ -25,14 +26,35 @@ class Feed extends Component {
   }
 
   render() {
-    console.log(this.props);
+    console.log(this.props.messages);
     return(
-      <Text>
-        Your messages!
-      </Text>
+      <ScrollView>
+        {
+          this.props.messages.map((message) => {
+            return (
+              <Message
+                body={message.body}
+                points={message.points}
+                shader={message.shader.username}
+                victim={message.victim.username}
+                status={message.message_status.name}
+                key={message.id}
+                style={styles.container}
+              />
+            )
+          })
+        }
+      </ScrollView>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: 40,
+    height: 40
+  }
+})
 
 const mapStateToProps = (state) => {
   return {
