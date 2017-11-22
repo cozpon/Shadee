80b126c9c19c6f1108f442e93624f98fdcde49d2
Ricky Peters <ericrickyallenpeters@gmail.com>
2017-11-22 12:05:48 -1000

    Swipe activated! Pink Buttons

--- app/actions/users.js
+++ app/actions/users.js
@@ -1,3 +1,9 @@
+import Axios from 'axios';
+
+const register = 'http://localhost:8080/api/auth/register';
+const login = 'http://localhost:8080/api/auth/login';
+const logout = 'http://localhost:8080/api/auth/logout';
+
 export const REGISTER_USER = 'REGISTER_USER';
 export const LOGIN_USER = 'LOGIN_USER';
 export const LOGOUT_USER = 'LOGOUT_USER';
@@ -7,44 +13,64 @@ export const LOAD_DETAIL_USERS = 'LOAD_DETAIL_USERS';
 export const LOAD_USERS = 'LOAD_USERS';
 export const DELETE_USER = 'DELETE_USER';
 
-const axios = require('axios');
-
-export const registerUser = (user) => {
-  return function(dispatch) {
-    return axios.post('http://localhost:8080/api/users', user).then((newUser) => {
+export const registerUser = (registerCreds) => {
+  return (dispatch) => {
+    return Axios.post(register, registerCreds)
+    .then(newUser => {
       dispatch({
         type: REGISTER_USER,
         user: newUser.data
       });
+    })
+    .catch(err => {
+      dispatch({
+        type: ERROR,
+        error: err
+      });
     });
-  }
-}
+  };
+};
 
-export const loginUser = (user) => {
-  return function(dispatch) {
-    return axios.post('http://localhost:8080/users/login', user).then((user) => {
+export const loginUser = (userCreds) => {
+  return (dispatch) => {
+    return Axios.post(login, userCreds)
+    .then(user => {
       dispatch({
         type: LOGIN_USER,
         user: user.data
       });
-    });
-  }
-}
-
-export const logoutUser = (user) => {
-  return function(dispatch) {
-    return axios.get('http://localhost:8080/users/logout').then((response) => {
+    })
+    .catch((err) => {
       dispatch({
-        type: LOGOUT_USER,
-        response: data.response
+        type: ERROR,
+        error: 'invalid user name or password'
       });
     });
-  }
-}
+  };
+};
+
+export const logoutUser = (user) => {
+  return (dispatch) => {
+    return Axios.get(logout)
+    .then(response => {
+      if(response.data.success) {
+        dispatch({
+          type: LOGOUT_USER,
+          success: response.data.success
+        });
+      }
+    })
+    .catch((err) => {
+      console.log('Logout failed. try again', err);
+      return false;
+      });
+    }
+  };
+
 
 export const loadUsers = () => {
   return function(dispatch) {
-    return axios.get('http://localhost:8080/api/users').then((users) => {
+    return Axios.get('http://localhost:8080/api/users').then((users) => {
       dispatch({
         type: LOAD_USERS,
         users: data.users
@@ -55,7 +81,7 @@ export const loadUsers = () => {
 
 export const loadDetailUsers = () => {
   return function(dispatch) {
-    return axios.get('http://localhost:8080/api/users/all').then((users) => {
+    return Axios.get('http://localhost:8080/api/users/all').then((users) => {
       dispatch({
         type: LOAD_DETAIL_USERS,
         users: data.users
@@ -66,7 +92,7 @@ export const loadDetailUsers = () => {
 
 export const loadUser = (id) => {
   return function(dispatch) {
-    return axios.get(`http://localhost:8080/api/users/${id}`).then((user) => {
+    return Axios.get(`http://localhost:8080/api/users/${id}`).then((user) => {
       dispatch({
         type: LOAD_USER,
         user: user.data
@@ -77,7 +103,7 @@ export const loadUser = (id) => {
 
 export const editUser = (user) => {
   return function(dispatch) {
-    return axios.put(`http://localhost:8080/api/users/${user.id}`).then((editedUser) => {
+    return Axios.put(`http://localhost:8080/api/users/${user.id}`).then((editedUser) => {
       dispatch({
         type: EDIT_USER,
         user: editedUser.data
