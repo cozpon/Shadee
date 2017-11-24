73ad79db9049eca6ef18eda03b5abb45d999420a
Ricky Peters <ericrickyallenpeters@gmail.com>
2017-11-24 10:26:55 -1000

    react-link fixed video

--- android/app/src/main/java/com/shadee/MainApplication.java
+++ android/app/src/main/java/com/shadee/MainApplication.java
@@ -3,6 +3,9 @@ package com.shadee;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.brentvatne.react.ReactVideoPackage;
+import com.rnfs.RNFSPackage;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +27,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactVideoPackage(),
+            new RNFSPackage(),
+            new RCTCameraPackage(),
             new VectorIconsPackage()
       );
     }
