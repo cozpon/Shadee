cc13ec9e015233a10ab3901111e81c56a562d5a4
Kaley Kriminger <kaley.kriminger@gmail.com>
2017-11-24 14:16:33 -1000

    localhost for convenience

--- app/lib/url.js
+++ app/lib/url.js
@@ -1,1 +1,4 @@
-export const url = 'http://10.0.1.8:8080/api/';
+//note: if you're running on your own computer, change this to http://localhost:8080/api/
+//if you're not, change it to like, http://10.0.1.8:8080/api/
+
+export const url = 'http://localhost:8080/api/';
