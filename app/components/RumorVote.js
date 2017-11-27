23bc774b7fcab56364c2d30e8883e867c6d77a13
Kaley Kriminger <kaley.kriminger@gmail.com>
2017-11-27 11:43:16 -1000

    buttons disappear after voting

--- app/components/RumorVote.js
+++ app/components/RumorVote.js
@@ -8,33 +8,43 @@ class RumorVote extends Component {
     super(props)
 
     this.state = {
-
+      voted: false
     }
   }
 
   _upvote(event){
     let rumor = {id : this.props.id, points : 1};
     this.props.editRumor(rumor);
+    this.setState({
+      voted: true
+    });
   }
 
   _downvote(event){
     let rumor = {id : this.props.id, points : 0};
     this.props.editRumor(rumor);
+    this.setState({
+      voted: true
+    });
   }
 
   render(){
     return(
       <View>
-        <Button
-          textStyle={{ color: "#ffb6c1" }}
-          title="Yeah, I heard that."
-          onPress={this._upvote.bind(this)}
-        />
-        <Button
-          textStyle={{ color: "#ffb6c1" }}
-          title="Nah, never heard that."
-          onPress={this._downvote.bind(this)}
-        />
+      { this.state.voted ? null :
+        <View>
+          <Button
+            textStyle={{ color: "#ffb6c1" }}
+            title="Yeah, I heard that."
+            onPress={this._upvote.bind(this)}
+          />
+          <Button
+            textStyle={{ color: "#ffb6c1" }}
+            title="Nah, never heard that."
+            onPress={this._downvote.bind(this)}
+          />
+        </View>
+      }
       </View>
 
     )
