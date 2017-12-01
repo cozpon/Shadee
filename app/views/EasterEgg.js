55594c9f7cc4208cdcd46b45e05234b73a1bc049
Ricky Peters <ericrickyallenpeters@gmail.com>
2017-11-30 16:43:58 -1000

    oops

--- app/views/EasterEgg.js
+++ app/views/EasterEgg.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { ScrollView, Text } from 'react-native';
+import { ScrollView, Text, Image } from 'react-native';
 
 class EasterEgg extends Component {
   render() {
     return (
         <ScrollView>
-          <Text> Welcome 2 Hell </Text>
+          <Text style={{size: 40, fontWeight: 'bold'}}> Welcome 2 Hell </Text>
+          <Image source={require('../assets/easteregg.jpg')}>
+          </Image>
         </ScrollView>
     )
   }
