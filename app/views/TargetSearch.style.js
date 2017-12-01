3284f7ae8b25806dcaf33bcef3e7c61943d5efef
Kaley Kriminger <kaley.kriminger@gmail.com>
2017-11-30 14:08:00 -1000

    remove clouds

--- app/views/TargetSearch.style.js
+++ app/views/TargetSearch.style.js
@@ -8,21 +8,19 @@ export default {
     borderWidth: 1,
     fontSize: 40,
     backgroundColor: 'white',
-    color: '#ffb6c1'
+    color: '#433D3F'
   },
   text: {
-    fontSize: 40
-  },
-  list: {
-    alignItems: 'center',
-    backgroundColor: 'transparent',
-    color: '#b5b8bc'
+    fontSize: 40,
+    color: '#433D3F'
   },
   backgroundImage: {
     flex: 1
   },
   transparent: {
-    backgroundColor: 'transparent',
     alignItems: 'center'
+  },
+  scrollView: {
+    backgroundColor: '#FFFFFF'
   }
 };
