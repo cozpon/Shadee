287d3981ff583de818b98c1610706a3444fa8c76
Kaley Kriminger <kaley.kriminger@gmail.com>
2018-02-04 19:50:00 -0800

    move delete button to Message component

--- app/components/Message.js
+++ app/components/Message.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import { Button, Icon } from 'react-native-elements';
 import Moment from 'react-moment';
 
-const Message = ({id, body, points, media, shader, victim, status, posted, flag}) => {
+const Message = ({deleteMessage, id, body, points, media, shader, victim, status, posted, flag}) => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>{points} upvotes. This shade is so {status}.</Text>
@@ -10,6 +11,15 @@ const Message = ({id, body, points, media, shader, victim, status, posted, flag}
       <Text style={styles.text}>{shader} threw shade @ {victim}</Text>
       <Text style={styles.body}>{body}</Text>
       <Text style={styles.moment}>{posted}</Text>
+      <Button
+        onPress={(e) => {
+          e.preventDefault();
+          deleteMessage(id);
+        }}
+        title={'Delete Shade'}
+        backgroundColor={'transparent'}
+        icon={{name: 'delete', color: '#433D3F'}}
+      />
     </View>
   )
 }
